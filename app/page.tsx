6697ac7f33513711c1c3abc08b132a0f99aea4f6
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2024-02-01 11:07:15 +0700

    update: adding custom window bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Layout from "@/components/layouts/Layout";
+
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
@@ -7,8 +9,12 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => router.push("/settings")}>Click me</Button>
-    </main>
+    <>
+      <Layout>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <Button onClick={() => router.push("/settings")}>Click me</Button>
+        </main>
+      </Layout>
+    </>
   );
 }
