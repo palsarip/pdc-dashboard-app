e1e53ccef842018dfa92870a38fb02041442ea72
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2025-02-17 10:03:25 +0700

    fix: fixing some changes

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
+import MainSidebar from "./MainSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,6 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Header />
       <div className="min-h-screen p-14 text-[#F2F3F5]">
         <Navbar />
+        <MainSidebar />
         {children}
       </div>
     </>
